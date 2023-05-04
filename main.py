0b3ef5c479e751b34f4f52388406dbcc714c029d
dimakuchinskyi <kuci.ia77@gmail.com>
2023-05-04 19:20:53 +0300

    Перший приклад атрибутів класів

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 #Класи
-'''
+
 class Student:
     print('Hi!')
+    count = 0
     def __init__(self, height = 150):
         self.height = height
+        Student.count += 1
 oleg = Student()#Об'єкт, екземпляр класу
 print(oleg.height)
 masha = Student(height = 200)
 print(masha.height)
-'''
-
+print(Student.count)
 
 
 
