f23ed9e72a80ef88bcca0e85738a6b5473d2cfe2
dimakuchinskyi <kuci.ia77@gmail.com>
2023-05-04 20:54:15 +0300

    Четверте завдання

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-#3
-class Animals:
-    pass
-class Cat(Animals):
-    def sound(self):
-        print('Мяу')
-bobik = Cat()
-bobik.sound()
+#4
+def __init__(self, owner, balance=0):
+    self.owner = owner
+    self.balance = balance
+
+
+def deposit(self, amount):
+    self.balance += amount
+    return f"Успішно внесено {amount} грн"
+
+
+def withdraw(self, amount):
+    if amount > self.balance:
+        return "Недостатньо коштів на рахунку"
+    else:
+        self.balance -= amount
+        return f"Успішно знято {amount} грн"
+
+
+def get_balance(self):
+    return f"Поточний  баланс: {self.balance} грн"
