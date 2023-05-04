3a641f6c870e4c0487c350987d69519b271799b5
dimakuchinskyi <kuci.ia77@gmail.com>
2023-05-04 20:44:03 +0300

    Третє завдання

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-#2
-class Circle:
-    def __init__(self, radius):
-        self.radius = radius
-
-    def area(self):
-        return 3.14 * (self.radius ** 2)
-
-my_circle = Circle(float(input("Введіть радіус кола:")))
-circle_area = my_circle.area()
-print(f"Площа кола = {circle_area}")
+#3
+class Animals:
+    pass
+class Cat(Animals):
+    def sound(self):
+        print('Мяу')
+bobik = Cat()
+bobik.sound()
