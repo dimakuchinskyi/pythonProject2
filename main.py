99f9a7f19313a994f5a10266223c4d9d0fe81afa
dimakuchinskyi <kuci.ia77@gmail.com>
2023-05-04 19:15:39 +0300

    Перший приклад атрибутів класів

---
+++ main.py
@@ -0,0 +1,15 @@
+#Класи
+'''
+class Student:
+    print('Hi!')
+    def __init__(self, height = 150):
+        self.height = height
+oleg = Student()#Об'єкт, екземпляр класу
+print(oleg.height)
+masha = Student(height = 200)
+print(masha.height)
+'''
+
+
+
+
