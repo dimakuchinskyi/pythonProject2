c658e3ae66f136195c1f569b88ec816a1e71bcfa
dimakuchinskyi <kuci.ia77@gmail.com>
2023-05-09 18:45:49 +0300

    Створений класс School,

--- main.py
+++ main.py
@@ -1,21 +1,8 @@
-#4
-def __init__(self, owner, balance=0):
-    self.owner = owner
-    self.balance = balance
-
-
-def deposit(self, amount):
-    self.balance += amount
-    return f"Успішно внесено {amount} грн"
-
-
-def withdraw(self, amount):
-    if amount > self.balance:
-        return "Недостатньо коштів на рахунку"
-    else:
-        self.balance -= amount
-        return f"Успішно знято {amount} грн"
-
-
-def get_balance(self):
-    return f"Поточний  баланс: {self.balance} грн"
+#Симулятор навчального закладу (база даних)
+class School:
+    def __init__(self, name, students):
+        self.name = name
+        self.students = students #Список студентів
+    def admit_student(self, student):
+        self.students.append(student)
+        print(f'{}') #Дописати, коли створено клас студентыв
